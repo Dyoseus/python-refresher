f3c43f68288ef901bbb6eec6ecf171d5113ef69d
Dyoseus <albert_h_le@hotmail.com>
2023-07-14 14:14:30 -0400

    physics functions

--- Physics/physics.py
+++ Physics/physics.py
@@ -1,24 +1,48 @@
-class Physics:
-    """def __init__(self, waterPressure, volume, gravity, depth):
-    self.waterPressure = waterPressure
-    self.volume = volume
-    self.gravity = gravity
-    self.depth = depth"""
+import numpy as np
+gravity = 9.81
+waterPressure = 1000
+pressureAtSurface = 101325
 
-    def calculate_buoyancy(density_fluid, volume):
-        buoyancy = density_fluid * volume
-        return buoyancy
+def calculate_buoyancy(density_fluid, volume):
+    """Buoyancy is calculated from multiplying density and volume"""
+    buoyancy = density_fluid * volume
+    return buoyancy
 
-    def will_it_float(volume, mass):
-        gravityForce = mass * 9.81
+def will_it_float(volume, mass):
+    """Mass is multiplied by gravity constant"""
+    gravityForce = mass * gravity
 
-        if 1000 * volume > gravityForce:
-            return True
-        else:
-            return False
+    if 1000 * volume > gravityForce:
+        return True
+    else:
+        return False
 
-    def calculate_pressure(depth):
-        waterPressure = 1000
-        gravity = 9.81
-        pressure = waterPressure * gravity * depth
-        return pressure
+def calculate_pressure(depth):
+    pressure = waterPressure * gravity * depth + pressureAtSurface
+    return pressure
+
+def calculate_acceleration(force, mass):
+    acceleration = force * mass
+    return mass
+
+def calculate_angular_acceleration(tau, I):
+    angularAcceleration = tau * I
+    return angularAcceleration
+
+def calculate_torque(F_magnitude, F_direction, r):
+    torque = F_magnitude * r * np.sin(F_direction)
+    return torque
+
+def calculate_moment_of_inertia(mass, r):
+    inertia = mass * r**2
+    return inertia
+
+def calculate_auv_acceleration(F_magnitude, F_angle, mass):
+    F_x = (F_magnitude * np.cos(F_angle)) / mass
+    F_y = (F_magnitude * np.sin(F_angle)) / mass
+    acceleration = np.sqrt(F_x**2 + F_y**2)
+    return acceleration
+
+def calculate_auv_angular_acceleration(F_magnitude, F_angle, inertia = 1, thruster_distance = 0.5):
+    F_magnitude * thruster_distance * math.sin(F_angle) / inertia
+
--- Physics/test_physics.py
+++ Physics/test_physics.py
@@ -1,19 +1,31 @@
 import unittest
-from physics import Physics
+import physics
 
 
 class TestPhysics(unittest.TestCase):
     def test_calculate_buoyancy(self):
-        self.assertEqual(Physics.calculate_buoyancy(3, 4), 12)
-        self.assertNotEqual(Physics.calculate_buoyancy(3, 4), 10)
+        self.assertEqual(physics.calculate_buoyancy(3, 4), 12)
+        self.assertNotEqual(physics.calculate_buoyancy(3, 4), 10)
 
     def test_calculate_float(self):
-        self.assertEqual(Physics.will_it_float(3, 4), True)
-        self.assertNotEqual(Physics.will_it_float(3, 4), False)
+        self.assertEqual(physics.will_it_float(3, 4), True)
+        self.assertNotEqual(physics.will_it_float(3, 4), False)
 
     def test_calculate_pressure(self):
-        self.assertEqual(Physics.calculate_pressure(50), 490500)
-        self.assertNotEqual(Physics.calculate_pressure(50), 500000)
+        self.assertEqual(physics.calculate_pressure(50), 490500)
+        self.assertNotEqual(physics.calculate_pressure(50), 500000)
+    
+    def test_calculate_acceleration(self):
+        self.assertEqual(physics.calculate_acceleration(10, 20), 200)
+        self.assertNotEqual(physics.calculate_acceleration(10, 20), 201)
+    def calculate_angular_acceleration(self):
+        self.assertEqual(physics.calculate_angular_acceleration(3,4), 12)
+        self.assertEqual(physics.calculate_angular_acceleration(3,4), 14)
+    def calculate_torque(self):
+        self.assertEqual(physics.calculate_torque(3, 45, 10), 10)
+        self.assertNotEqual()
+    def calculate_moment_of_inertia(self):
+
 
 
 if __name__ == "__main__":
